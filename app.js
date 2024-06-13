7709ac4ef72e37ccc7c80edfb56a8df5606f883e
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-13 16:15:54 +0100

    update

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const mouse = document.querySelector('#mouse')
-mouse.addEventListener('mousemove', (e) => {})
+const mouse = document.querySelector('#mouse');
+
+mouse.addEventListener('mousemove', (e) => {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+});
