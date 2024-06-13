73c24b3f135603b9a8e29af21c950651c94a5b50
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-13 18:27:21 +0100

    update

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ window.addEventListener('mousemove', (e) => {
     mouse.style.top = e.pageY + "px";
     mouse.style.left = e.pageX + "px";
 });
+
+window.addEventListener('mouseleave', () => {
+    mouse.style.display = 'none';  
+});
+
+window.addEventListener('mouseenter', () => {
+    mouse.style.display = 'block';  // Faire réapparaître la div quand la souris entre dans la fenêtre
+});
