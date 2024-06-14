1cd0b63a70e98b5bed0bee5bd0bc3341cb8d7f1c
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-14 09:49:10 +0100

    update

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ window.addEventListener('mousemove', (e) => {
     mouse.style.top = e.pageY + "px";
     mouse.style.left = e.pageX + "px";
 });
+
+const Tittles = document.querySelectorAll('h1, h3');
+Tittles.addEventListener('mousemove', (e) => {
+    mouse.style.transform = scale(1.1)
+})
